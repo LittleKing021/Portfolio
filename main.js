96c524b6c38975ad8c719d4c91c795e496a32306
POSEIDON <northchumash@protonmail.com>
2025-03-13 18:46:48 -0400

    My Portfolio

---
+++ main.js
@@ -0,0 +1,8 @@
+let niggaName = "LUCKY"
+let niggaAge = 17
+
+if (niggaAge === 'LUCKY' || niggaAge === 17) {
+    console.log('YOU HAVE HIT THAT UNC STAGE GNG.')
+} else {
+    console.log('SORRY UR STILL A BABY.')
+}
