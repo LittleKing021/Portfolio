d5c5daf1d6bdbbef9968806164b3b219732a88a6
POSEIDON <northchumash@protonmail.com>
2025-03-21 09:39:19 -0400

    'Update"

--- main.js
+++ main.js
@@ -1,8 +1,3 @@
-let niggaName = "LUCKY"
-let niggaAge = 17
-
-if (niggaAge === 'LUCKY' || niggaAge === 17) {
-    console.log('YOU HAVE HIT THAT UNC STAGE GNG.')
-} else {
-    console.log('SORRY UR STILL A BABY.')
-}
+//
+UPDATE
+//
